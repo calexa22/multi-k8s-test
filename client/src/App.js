db2c1220d49470b894f3a69c09d62f4c95f075cd
Chris Alexander <cea2cool@gmail.com>
2021-10-03 19:04:59 -0500

    pr commit test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator - Hosted on Kubernetes
+            Fib Calculator - Running in Kubernetes!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
